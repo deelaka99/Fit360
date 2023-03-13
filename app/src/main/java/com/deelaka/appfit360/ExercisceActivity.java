513c36a24be8094e79b429514b419df1578c7b23
deelaka99 <123wejith@gmail.com>
2023-03-13 18:57:40 +0530

    Finished UI of Home activity

---
+++ app/src/main/java/com/deelaka/appfit360/ExercisceActivity.java
@@ -0,0 +1,14 @@
+package com.deelaka.appfit360;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExercisceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercisce);
+    }
+}
