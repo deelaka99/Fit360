6b040d03f0573dda67e82154050dd9e10f64aa83
deelaka99 <123wejith@gmail.com>
2023-03-13 12:10:42 +0530

    Finished UI of Edit profile activity

--- app/src/main/java/com/deelaka/appfit360/HomeActivity.java
+++ app/src/main/java/com/deelaka/appfit360/HomeActivity.java
@@ -2,7 +2,10 @@ package com.deelaka.appfit360;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnEditProfile = findViewById(R.id.btnEditProfile);
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
