37a585f4b60e966d523b3e4e97c297bb46d9c56f
deelaka99 <123wejith@gmail.com>
2023-03-15 11:54:46 +0530

    Added alert box to the exercise activity to edit the plan limit

--- app/src/main/java/com/deelaka/appfit360/ExercisceActivity.java
+++ app/src/main/java/com/deelaka/appfit360/ExercisceActivity.java
@@ -2,7 +2,10 @@ package com.deelaka.appfit360;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExercisceActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class ExercisceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercisce);
+
+        //For the View map button
+        Button btnViewMap = findViewById(R.id.btnViewMap);
+        btnViewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ExercisceActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //For the btnWalEdiPla button
+        Button btnWalEdiPla = findViewById(R.id.btnWalEdiPla);
+        btnWalEdiPla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog(){
+        Dialog exampleDialog = new Dialog();
+        exampleDialog.show(getSupportFragmentManager(),"Example Dialog");
     }
 }
