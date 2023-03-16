f4ae6ee773acc503e4d26315b9adcc8e36cdc38b
deelaka99 <123wejith@gmail.com>
2023-03-16 20:41:14 +0530

    Finalized the UI with Food Tracker UI

--- app/src/main/java/com/deelaka/appfit360/HomeActivity.java
+++ app/src/main/java/com/deelaka/appfit360/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.deelaka.appfit360.foodTracker.FoodTrackerActivity;
 import com.deelaka.appfit360.waterTracker.WaterTrackerActivity;
 
 public class HomeActivity extends AppCompatActivity {
@@ -37,13 +38,10 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         //For Start logging your food's Go button
-//        Button btnEx3 = findViewById(R.id.btnEx3);
-//        btnEx3.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(HomeActivity.this, WaterTrackerActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        Button btnEx3 = findViewById(R.id.btnEx3);
+        btnEx3.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, FoodTrackerActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/deelaka/appfit360/foodTracker/FoodTrackerActivity.java
+++ app/src/main/java/com/deelaka/appfit360/foodTracker/FoodTrackerActivity.java
@@ -1,4 +1,4 @@
-package com.deelaka.appfit360.waterTracker;
+package com.deelaka.appfit360.foodTracker;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +8,12 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.deelaka.appfit360.R;
-import com.deelaka.appfit360.ViewPagerFragmentAdapter;
+import com.deelaka.appfit360.ViewPagerFragmentAdapter1;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.Objects;
-
-public class WaterTrackerActivity extends AppCompatActivity {
-    ViewPagerFragmentAdapter viewPagerFragmentAdapter;
+public class FoodTrackerActivity extends AppCompatActivity {
+    ViewPagerFragmentAdapter1 viewPagerFragmentAdapter1;
     TabLayout tabLayout;
     ViewPager2 viewPager2;
     String[] titles = new String[]{"Home","History"};
@@ -23,19 +21,19 @@ public class WaterTrackerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_water_tracker);
+        setContentView(R.layout.activity_food_tracker);
 
         //Formatting action bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_200)));
-        getSupportActionBar().setTitle("Fit360 Water Tracker");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.green)));
+        getSupportActionBar().setTitle("Fit360 Food Tracker");
 
         tabLayout = findViewById(R.id.tabLayout);
         viewPager2 = findViewById(R.id.viewPager);
-        viewPagerFragmentAdapter = new ViewPagerFragmentAdapter(this);
+        viewPagerFragmentAdapter1 = new ViewPagerFragmentAdapter1(this);
 
-        viewPager2.setAdapter(viewPagerFragmentAdapter);
+        viewPager2.setAdapter(viewPagerFragmentAdapter1);
 
         new TabLayoutMediator(tabLayout,viewPager2,((tab, position) -> tab.setText(titles[position]))).attach();
     }
